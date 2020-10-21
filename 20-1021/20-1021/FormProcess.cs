1ef93ccd78ad2418cd8c6240a36590e426812c96
chanos <chanos.dev@gmail.com>
2020-10-21 21:44:37 +0900

    20-1021

---
+++ 20-1021/20-1021/FormProcess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20_1021
+{
+    public partial class FormProcess : Form
+    {
+        public FormProcess()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Process_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox_Process.Text))
+                return;
+
+            Process.Start(textBox_Process.Text);
+        }
+    }
+}
