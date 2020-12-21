b6238e42330afb8b5bb7e4c7cb4553b76cdc99e9
chanos-dev <chanos.dev@gmail.com>
2020-12-21 21:10:45 +0900

    20-1216

---
+++ 20-1216/20-1216/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_1216
+{
+    class Program
+    {
+        static Stopwatch stopwatch = new Stopwatch();
+        
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int temp;
+
+            stopwatch.Reset();
+            a = 10;
+            b = 20;
+
+            stopwatch.Start();
+            for (int i = 0; i < 100000001; i++)
+            {
+                temp = a;
+                a = b;
+                b = temp;
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"temp : {a}, {b}");
+            Console.WriteLine(stopwatch.Elapsed);
+
+            stopwatch.Reset();
+            a = 10;
+            b = 20;
+
+            stopwatch.Start();
+            for (int i = 0; i < 100000001; i++)
+            {
+                a ^= b ^= a;
+                b ^= a;
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"xor : {a}, {b}");
+            Console.WriteLine(stopwatch.Elapsed);
+
+            stopwatch.Reset();
+            a = 10;
+            b = 20;
+
+            stopwatch.Start();
+            for (int i = 0; i < 100000001; i++)
+            {
+                a += b;  
+                b = a - b;  
+                a -= b;
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"add, sub : {a}, {b}");
+            Console.WriteLine(stopwatch.Elapsed);
+        }
+    }
+}
