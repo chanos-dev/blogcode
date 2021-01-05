be54b3fb0f826538603c17301dc447a6b3f79a0b
chanos-dev <chanos.dev@gmail.com>
2021-01-05 22:18:04 +0900

    21-0104

---
+++ 21-0104/21-0104/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_0104
+{
+    static class ExtensionClass
+    {
+        public static int Factorial(this int i)
+        {
+            if (i < 2)
+                return 1;
+            else
+                return i * Factorial(i - 1);
+        }
+
+        public static string StringConcat(this string str, string str2)
+        {
+            return str + str2;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inumber;
+            Console.Write("Factorial : ");
+            if (int.TryParse(Console.ReadLine(), out inumber))
+                Console.WriteLine(inumber.Factorial().ToString());
+            else
+                Console.WriteLine("Input Error"); 
+
+            string str = "Hello";
+            Console.WriteLine(str.StringConcat(" World"));  
+        }
+    }
+}
