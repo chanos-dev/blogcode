dc40f5b3901caec84b6a597054ede992858da8f3
chanos-dev <chanos.dev@gmail.com>
2021-03-11 00:21:51 +0900

    21-0311

---
+++ 21-0311/21-0311/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_0311
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var properties = typeof(Person).GetProperties();
+
+            foreach (var property in properties)
+            {
+                Console.WriteLine(property.Name);
+            }
+
+            Console.WriteLine("");
+
+            var skipProperties = typeof(Person).GetProperties().Where(property => !Attribute.IsDefined(property, typeof(SkipPropertyAttribute)));
+
+            foreach (var skip in skipProperties)
+            {
+                Console.WriteLine(skip.Name);
+            }
+        }
+    }
+
+    class SkipPropertyAttribute : Attribute
+    {
+
+    }
+
+    class Person
+    {
+        [SkipProperty]
+        public string PersonalID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; } 
+        public string Address { get; set; } 
+    }
+}
