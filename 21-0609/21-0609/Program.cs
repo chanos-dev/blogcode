6b426fc801d3d181827b37c4e49a92572a265c7e
chanos-dev <chanos.dev@gmail.com>
2021-06-09 23:34:31 +0900

    21-0609

---
+++ 21-0609/21-0609/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_0609
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var actions = new List<Action>();
+
+            for (int idx = 0; idx < 5; idx++)
+            {
+                //actions.Add(() => Console.WriteLine(idx));
+
+                int temp = idx;
+                actions.Add(() => Console.WriteLine(temp));
+            }
+
+            foreach (var action in actions)
+            {
+                action.Invoke();
+            }
+        }
+    }
+}
