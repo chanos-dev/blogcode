48ef86fd901ab4a41ac05857ba16be70907348df
chanos-dev <chanos.dev@gmail.com>
2021-07-27 21:50:24 +0900

    21-0726

---
+++ 21-0726/21-0726/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_0726
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parent child = new Child();
+            Compare(child);
+        }
+
+        private static void Compare(Parent obj)
+        {
+            Parent newChild = new Child();
+            // false
+            Console.WriteLine(obj.GetType() == typeof(Parent));
+            // true
+            Console.WriteLine(obj.GetType() == typeof(Child));
+            // true
+            Console.WriteLine(obj.GetType() == newChild.GetType());
+        }
+    }
+
+    class Parent { }
+
+    class Child : Parent { }
+}
