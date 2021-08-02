fadada02ec28d2a7ee375010062cc87759aa5a56
chanos-dev <chanos.dev@gmail.com>
2021-08-02 23:25:25 +0900

    20-0802

---
+++ 20-0802/20-0802/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_0802
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo = new Foo();
+
+        }
+    }
+
+    class Foo
+    {
+        private StreamWriter sw { get; set; }
+
+        public Foo()
+        {
+            sw = new StreamWriter(@"C:\Users\Chanos\Desktop\git\test.txt");
+        }
+
+        ~Foo()
+        {
+            sw?.Close();
+        }
+    }
+
+}
