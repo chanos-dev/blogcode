8b53d077e03ecf9a9e4772c79ff1a489f637bd43
chanos-dev <chanos.dev@gmail.com>
2021-11-09 01:33:04 +0900

    21-1109

---
+++ 21-1109/21-1109/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_1109
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo = new Foo()
+            {
+                Value = "Hello, World",
+            };
+
+            var binaryFormatter = new BinaryFormatter();
+
+            using (var stream = File.Open("hello.txt", FileMode.Create))
+            {
+                binaryFormatter.Serialize(stream, foo);
+                Console.WriteLine(foo.Value);
+            }
+
+            using (var stream = File.Open("hello.txt", FileMode.Open))
+            {
+                var newFoo = binaryFormatter.Deserialize(stream) as Foo;
+                Console.WriteLine(newFoo.Value);
+            }
+
+            using (var ms = new MemoryStream())
+            {
+                binaryFormatter.Serialize(ms, foo);
+
+                Console.WriteLine(Encoding.UTF8.GetString(ms.ToArray()));
+            }
+        }
+    }
+
+    [Serializable]
+    internal class Foo
+    {
+        internal string Value { get; set; }
+    }
+}
