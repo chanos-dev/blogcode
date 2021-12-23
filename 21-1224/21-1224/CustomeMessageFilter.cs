9c8314485a6cac86cb2312524346a92cac96a6c7
chanos-dev <chanos.dev@gmail.com>
2021-12-24 00:46:33 +0900

    21-1224

---
+++ 21-1224/21-1224/CustomeMessageFilter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _21_1224
+{
+    internal class CustomMessageFilter : IMessageFilter
+    {
+        private ListBox ListBox { get; set; }
+
+        public CustomMessageFilter(ListBox listBox)
+        {
+            this.ListBox = listBox;
+        }
+
+        public bool PreFilterMessage(ref Message m)
+        {
+            if (m.Msg == 0x201)
+            {
+                ListBox.Items.Add($"선 처리!! | {m}");
+                // return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ 21-1224/21-1224/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _21_1224
+{
+    public partial class Form1 : Form
+    {
+        private IMessageFilter Filter { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            Filter = new CustomMessageFilter(this.listBox1);
+            Application.AddMessageFilter(Filter);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (var form = new Form2())
+            {
+                //Application.RemoveMessageFilter(Filter);
+                form.StartPosition = FormStartPosition.CenterParent;
+                form.ShowDialog();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("버튼 클릭!!");
+        }
+    }
+}
