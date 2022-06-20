86aa1211f91ec68414dff9582633359fa7b27b2e
chanos-dev <chanos.dev@gmail.com>
2022-06-21 02:00:14 +0900

    22-0620

---
+++ 22-0620/22-0620/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_0620
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Capacity();
+            NonCapacity();
+        }
+
+        private static void Capacity()
+        {
+            List<int> list = new List<int>(1 << 26);
+            list.AddRange(Enumerable.Range(1, 1 << 26));
+            Console.WriteLine($"capacity : {list.Capacity}, count : {list.Count}");
+            list.Add(-1);
+            Console.WriteLine($"capacity : {list.Capacity}, count : {list.Count}");
+        }
+
+        private static void NonCapacity()
+        {
+            List<int> list = new List<int>();
+            list.AddRange(Enumerable.Range(1, 1 << 26));
+            Console.WriteLine($"capacity : {list.Capacity}, count : {list.Count}");
+            list.Add(-1);
+            Console.WriteLine($"capacity : {list.Capacity}, count : {list.Count}");
+        }
+    }
+}
