f28c0a384e5e92be74110be371b9702377a38a9a
chanos-dev <chanos.dev@gmail.com>
2023-11-15 01:54:44 +0900

    23-1115

---
+++ 23-1115/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+
+WebProxy proxy = new("192.168.0.105", 3128)
+{
+    Credentials = new NetworkCredential("test1", "test1"),
+};
+
+HttpClientHandler handler = new()
+{
+    Proxy = proxy,
+};
+
+try
+{
+    using HttpClient client = new(handler);
+    using var response = await client.GetAsync("https://microsoft.com/");
+    //deny
+    using var responseDeny = await client.GetAsync("https://www.naver.com/");
+}
+catch (HttpRequestException ex)
+{
+    // deny exception 403
+    // auth exception 407
+    Console.WriteLine(ex);
+}
